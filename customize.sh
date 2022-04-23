573463bcefd9b6e982997d50d54027ce7cd61ae0
ToDesk <89590950+ToDesk@users.noreply.github.com>
2022-04-23 16:15:15 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -6,10 +6,16 @@
 # Youtube Channel: https://goo.gl/fvkdwm 
 #=================================================
 #1. Modify default IP
-#sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
-
+#
+sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
 #2. Clear the login password
 #sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
 
 #3. Replace with JerryKuKu’s Argon
 rm openwrt/package/lean/luci-theme-argon -rf
+
+#4. 修改插件名称
+sed -i 's/"Turbo ACC 网络加速"/"网络加速"/g' openwrt/package/lean/luci-app-sfe/po/zh-cn/sfe.po
+
+#5. 修改默认主题
+sed -i 's/"luci-theme-bootstrap"/"luci-theme-argonne"/g' openwrt/feeds/luci/collections/luci/Makefile
